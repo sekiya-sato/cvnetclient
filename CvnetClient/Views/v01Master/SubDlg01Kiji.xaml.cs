8fe8c7ece09e3e19081b671001670bfce180e784
BellaZull <nabilahbintizulkifli@gmail.com>
2025-08-29 09:44:23 +0800

    Nabilah：生地付属マスターのViewを追加
    デザインはまだ作成中の段階です。RidzwanさんのTemplateをPullして、デザインを続きたいと思います。

---
+++ CvnetClient/Views/v01Master/SubDlg01Kiji.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CvnetClient.Views.v01Master
+{
+    /// <summary>
+    /// Interaction logic for SubDlg01Kiji.xaml
+    /// </summary>
+    public partial class SubDlg01Kiji : Window
+    {
+
+        public SubDlg01Kiji()
+        {
+            InitializeComponent();
+        }
+    }
+}
