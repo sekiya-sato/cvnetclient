5c24543a20301833712ae7096da2ad4ed5894a39
RidzwanZul <81953173+RidzwanZul@users.noreply.github.com>
2025-09-11 14:59:08 +0800

    MasterMeisho design

--- CvnetClient/Models/PageConfig.cs
+++ CvnetClient/Models/PageConfig.cs
@@ -12,7 +12,7 @@ namespace CvnetClient.Models
     { 
         { "マスタ", new PageData {
             Buttons = new List<ButtonConfig> {
-                new ButtonConfig { Text = "名称マスタ", WindowId= typeof(Views.MasterMeishoView).FullName },
+                new ButtonConfig { Text = "名称マスタ", WindowId= typeof(Views.SubDlg01MeiView).FullName },
                 new ButtonConfig { Text = "得意先マスタ", WindowId="" },
                 new ButtonConfig { Text = "仕入マスタ", WindowId="" },
                 new ButtonConfig { Text = "商品マスタ(新規登録)", WindowId= typeof(Views.SubDlg01Sho2View).FullName },
--- CvnetClient/Views/v01Master/SubDlg01MeiView.xaml.cs
+++ CvnetClient/Views/v01Master/SubDlg01MeiView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace CvnetClient.Views.v01Master
+namespace CvnetClient.Views
 {
     /// <summary>
     /// Interaction logic for SubDlg01MeiView.xaml
