61e5d20ed6b35fc43dae1651f0cc818be67eec5f
RidzwanZul <81953173+RidzwanZul@users.noreply.github.com>
2025-10-22 15:25:34 +0800

    名称マスタコピー作成

---
+++ CvnetClient/ViewModels/v01Master/SubDlg01MeiConvViewModel.cs
@@ -0,0 +1,119 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CvnetBaseCore;
+using CvnetClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Converters;
+
+namespace CvnetClient.ViewModels
+{
+    public partial class SubDlg01MeiConvViewModel : BaseViewModel
+    {
+
+        [ObservableProperty]
+        ObservableCollection<Templ>? copyFrom;
+        [ObservableProperty]
+        ObservableCollection<Templ>? copyTo;
+        [ObservableProperty]
+        Templ? selectedCopyFrom;
+        [ObservableProperty]
+        Templ? selectedCopyTo;
+
+        string sql = """"
+            select 名称CD,名称CD||' '||名称,'HC$MASTER_MEISHO' マスタ名,'名称区分='''||名称CD||'''' aaa from hc$master_meisho where 名称区分='IDX' order by 名称CD
+            """";
+        public void OnInit() {
+            var retData = AppData.Http?.AspxSqlQuery(sql, new string[] {});
+            if (retData == null || retData.Rows.Count == 0) return;
+            var list = (from DataRow dr in retData.Rows
+                        select new Templ
+                        {
+                            Value = dr["名称CD"].ToString() ?? string.Empty,
+                            Title = dr["名称CD||''||名称"].ToString() ?? string.Empty,
+                            Tb_name = dr["マスタ名"].ToString() ?? string.Empty,
+                            Where_str = dr["aaa"].ToString() ?? string.Empty
+                        }).ToList();
+            Common.ConvertDotStringDel(list);
+            CopyTo = new ObservableCollection<Templ>(list);
+            
+            var staticList = new List<Templ>
+            {
+                new Templ { Title = "得意先マスタ", Tb_name = "HC$MASTER_TOKUI" },
+                new Templ { Title = "仕入マスタ", Tb_name = "HC$MASTER_SIIRE" },
+                new Templ { Title = "商品マスタ", Tb_name = "HC$MASTER_SHOHIN" },
+                new Templ { Title = "委託倉庫", Tb_name = "HC$MASTER_TOKUI", Where_str = "店種区分=0 and 倉庫区分=2" },
+                new Templ { Title = "店舗", Tb_name = "HC$MASTER_TOKUI", Where_str = "店種区分 in (3,6)" },
+                new Templ { Title = "社員マスタ", Tb_name = "HC$MASTER_SHAIN" }
+            };
+            list.InsertRange(0, staticList);
+            CopyFrom = new ObservableCollection<Templ>(list);
+        }
+
+        [RelayCommand]
+        void DoCopy() 
+        {
+            if (SelectedCopyFrom == null) return;
+            if(SelectedCopyTo == null) return;
+            if (!ClientLib.MessageBox(this, SelectedCopyFrom.Title + "から" + SelectedCopyTo.Title + "にコピーを実行します\nよろしいですか？")) return;
+
+            string[] v_para = new string[4];
+            v_para[0] = SelectedCopyFrom.Tb_name;
+            var col_str = string.Empty;
+            if (v_para[0] == "HC$MASTER_TOKUI") 
+            {
+                col_str = "得意先CD,得意先名,略称";
+            }
+            else if (v_para[0] == "HC$MASTER_SIIRE")
+            {
+                col_str = "仕入先CD,仕入先名,略称";
+            }
+            else if (v_para[0] == "HC$MASTER_SHOHIN")
+            {
+                col_str = "商品CD,商品名";
+            }
+            else if (v_para[0] == "HC$MASTER_MEISHO")
+            {
+                col_str = "名称CD,名称,略称";
+            }
+            else if (v_para[0] == "HC$MASTER_SHAIN")
+            {
+                col_str = "社員CD,名前,フリガナ";
+            }
+            v_para[1] = col_str;
+            v_para[2] = SelectedCopyFrom.Where_str;
+            v_para[3] = SelectedCopyTo.Value;
+
+            var ret = AppData.Http!.AspxSqlQuery2("Mei_Copy", v_para, "", -1);
+            if (ret.Split(',')[0].Trim() != "0")
+            {
+                ClientLib.MessageBoxError(this, "更新エラー");
+            }
+            else {
+                ClientLib.MessageBoxOk(this, "正常更新終了");
+            }
+            
+        }
+
+        public partial class Templ : ObservableObject
+        {
+
+            [ObservableProperty]
+            public string? value;
+
+            [ObservableProperty]
+            public string? title;
+
+            [ObservableProperty]
+            public string? tb_name;
+
+            [ObservableProperty]
+            public string? where_str;            
+        }
+    }
+}
---
+++ CvnetClient/ViewModels/v999Others/SubDlg30InpMbiViewModel.cs
@@ -0,0 +1,122 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CvnetBaseCore;
+using CvnetClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using static CvnetClient.ViewModels.SubDlg01MeiConvViewModel;
+using static CvnetClient.ViewModels.SubDlg80GphABC2aViewModel;
+
+namespace CvnetClient.ViewModels
+{
+    public partial class SubDlg30InpMbiViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        ObservableCollection<Budget>? budgetList;
+        [ObservableProperty]
+        public int? sum;
+        [ObservableProperty]
+        Search? searchCond;
+
+        public void OnInit() {
+            SearchCond = new Search();
+            SearchCond.ItemFrom = "zzzzzzzzzzzzzz";
+            //SearchCond.Date = DateOnly.TryParse(DateTime.Now.ToString(), out null);
+        }
+
+        [RelayCommand]
+        void DoSearch() {
+            int strFLG = 0;
+            string strSql = "";
+            //var ret_csv = null;
+            string[] wrk_para = new string[5];
+            wrk_para[0] = SearchCond?.Date?.ToString("yyyyMMdd");
+            wrk_para[1] = ".";
+            wrk_para[2] = SearchCond.Brd;
+            wrk_para[3] = SearchCond.ItemFrom;
+            wrk_para[4] = SearchCond.ItemTo;
+
+            if (wrk_para[3] == "")
+            {
+                wrk_para[3] = ".";
+            }
+
+            if (wrk_para[4] == "")
+            {
+                wrk_para[3] = "zzzzzzzzzzzzzz";
+            }
+
+
+            strSql = "SELECT A.名称CD,A.名称,nvl(B.予算数量,0) AS su ,nvl(B.予算金額,0) AS kin ";
+            strSql += "FROM HC$MASTER_MEISHO A LEFT OUTER JOIN HC$MASTER_YO_ITEM B ";
+            strSql += " ON A.名称CD = B.アイテムCD ";
+            strSql += "WHERE A.名称区分 = 'ITM'";
+            strSql += " AND A.ランク != '.'";
+            strSql += " AND 店舗CD = '" + wrk_para[1] + "'";
+            strSql += " AND 日付 = '" + wrk_para[0] + "'";
+            strSql += " AND ブランドCD = '" + wrk_para[2] + "'";
+            strSql += " AND アイテムCD between '" + wrk_para[3] + "' and '" + wrk_para[4] + "'";
+            strSql += " union";
+            strSql += " SELECT 名称CD,名称,0 AS su ,0 AS kin ";
+            strSql += "FROM HC$MASTER_MEISHO ";
+            strSql += "WHERE 名称区分 = 'ITM'";
+            strSql += " AND ランク != '.'";
+            strSql += " AND 名称CD between '" + wrk_para[3] + "' and '" + wrk_para[4] + "'";
+
+
+            string strSql2 = "";
+            strSql2 = " select A.名称CD, A.名称, sum(A.su) su, sum(A.kin) kin ";
+            strSql2 += " from (" + strSql + ") a";
+            strSql2 += " group by a.名称CD, a.名称";
+            strSql2 += " order by 名称CD ";
+
+            strSql = strSql2;
+
+            var ret_csv = AppData.Http!.AspxSqlQuery(strSql);
+
+            var list = (from DataRow dr in ret_csv.Rows
+                        select new Budget
+                        {
+                            ItemCD = dr["名称CD"].ToString() ?? string.Empty,
+                            ItemName = dr["名称"].ToString() ?? string.Empty
+                        }).ToList();
+            Common.ConvertDotStringDel(list);
+            BudgetList = new ObservableCollection<Budget>(list);
+
+        }
+
+        [RelayCommand]
+        void DoSum() 
+        { 
+        
+        }
+    }
+
+    public partial class Budget : ObservableObject 
+    {
+        [ObservableProperty]
+        public string? itemCD;
+        [ObservableProperty]
+        public string? itemName;
+        [ObservableProperty]
+        public int? budgetPrice;
+    }
+
+    public partial class Search : ObservableObject 
+    {
+        [ObservableProperty]
+        public DateOnly? date;
+        [ObservableProperty]
+        public string? brd;
+        [ObservableProperty]
+        public string? itemFrom;
+        [ObservableProperty]
+        public string? itemTo;
+    }
+}
