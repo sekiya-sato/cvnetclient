587d629a021f041a6bda4ef15a1e09a81f3b0e73
RidzwanZul <81953173+RidzwanZul@users.noreply.github.com>
2025-10-23 11:26:18 +0800

    バーコードブック発行

---
+++ CvnetClient/ViewModels/v05Exhibition/SubDlg05PrnBcbookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvnetClient.ViewModels
+{
+    public enum OutPutType { 正規商品, 中止商品CD, 全て }
+    public enum BarcodeType2 { JAN,CODE39, NW7 }
+    public partial class SubDlg05PrnBcbookViewModel : BaseViewModel
+    {
+        public void OnInit() 
+        { 
+        
+        }
+    }
+
+    public partial class Condition : ObservableObject 
+    { 
+    
+    
+    }
+}
