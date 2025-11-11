b7bfc43f0c248abd1de7ef119f8c84feaecd84d3
BellaZull <nabilahbintizulkifli@gmail.com>
2025-11-11 10:44:22 +0800

    Convert Master Design

--- CvnetClient/ViewModels/v01Master/SubDlg01ConvertViewModel.cs
+++ CvnetClient/ViewModels/v01Master/SubDlg01ConvertViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CvnetBaseCore;
+using System.Data;
+using CvnetClient.Models;
+using CvnetClient.Views;
+using System.Collections.ObjectModel;
+using CvnetClient.Utils;
 
-namespace CvnetClient.ViewModels.v01Master
+namespace CvnetClient.ViewModels
 {
-    internal class SubDlg01ConvertViewModel
+    public partial class SubDlg01ConvertViewModel : BaseViewModel
     {
+
     }
 }
