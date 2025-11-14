519114bc737e25f8f90cc814433f445816f66d25
RidzwanZul <81953173+RidzwanZul@users.noreply.github.com>
2025-11-14 17:21:26 +0800

    発注入力 update

--- CvnetClient/ViewModels/v999Others/SubDlg30InpMbiViewModel.cs
+++ CvnetClient/ViewModels/v999Others/SubDlg30InpMbiViewModel.cs
@@ -24,8 +24,9 @@ namespace CvnetClient.ViewModels
             {
                 OnInitBase(init_para, init_flg);
                 SearchCond = new Search();
-            SearchCond.Date = DateTime.Now;
-            SearchCond.ItemTo = "zzzzzzzzzzzzzz";
+                SearchCond.Date = DateTime.Now;
+                SearchCond.ItemTo = "zzzzzzzzzzzzzz";
+            }
             //SearchCond.Date = DateOnly.TryParse(DateTime.Now.ToString(), out null);
         }
         #endregion
