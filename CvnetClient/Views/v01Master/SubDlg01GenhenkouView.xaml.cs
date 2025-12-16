255f928dc26f63d8602a7c372acae70024e272dc
BellaZull <nabilahbintizulkifli@gmail.com>
2025-12-16 09:54:18 +0800

    MasterGenkaHenkou
    マスタ原価変更の作成：－
    編集中

--- CvnetClient/Views/v01Master/SubDlg01GenhenkouView.xaml.cs
+++ CvnetClient/Views/v01Master/SubDlg01GenhenkouView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CvnetClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
 
-namespace CvnetClient.Views.v01Master
+namespace CvnetClient.Views
 {
     /// <summary>
     /// Interaction logic for SubDlg01GenhenkouView.xaml
@@ -23,5 +25,41 @@ namespace CvnetClient.Views.v01Master
         {
             InitializeComponent();
         }
+
+        //private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        //{
+        //    // Get the index
+        //    int index = e.Row.GetIndex() + 1;
+
+        //    // Find the TextBlock in the first column
+        //    if (e.Row.Item is object dataItem)
+        //    {
+        //        //var textBlock =
+        //        //    ListDetail.Columns[0].GetCellContent(e.Row) as TextBlock;
+
+        //        //if (textBlock != null)
+        //        //{
+        //        //    textBlock.Text = index.ToString();
+        //        //}
+        //        var grid = ListDetail.Columns[0].GetCellContent(e.Row) as Grid;
+        //        if (grid != null)
+        //        {
+        //            var tb = grid.Children.OfType<TextBlock>().FirstOrDefault();
+        //            if (tb != null)
+        //                tb.Text = (e.Row.GetIndex() + 1).ToString();
+        //        }
+
+        //    }
+        //}
+
+        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is SubDlg01GenhenkouViewModel vm &&
+                vm.DoDetailListCommand.CanExecute(null))
+            {
+                vm.DoDetailListCommand.Execute(null);
+            }
+        }
+
     }
 }
