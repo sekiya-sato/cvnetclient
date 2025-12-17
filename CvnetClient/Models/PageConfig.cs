dcdd7b1504625a1598094a352e61187ab907a3f1
BellaZull <nabilahbintizulkifli@gmail.com>
2025-12-17 15:03:47 +0800

    SetJanView作成

--- CvnetClient/Models/PageConfig.cs
+++ CvnetClient/Models/PageConfig.cs
@@ -36,7 +36,7 @@ namespace CvnetClient.Models
                 new ButtonConfig { Text = "名称マスタコピー作成", WindowId=typeof(Views.SubDlg01MeiConvView).FullName,Parameter="0",Flag="0" },
                 new ButtonConfig { Text = "取込レイアウト作成", WindowId=typeof(Views.SubDlg00InputCsvView).FullName },
                 new ButtonConfig { Text = "外部CSVデータ取込", WindowId=typeof(Views.SubDlg01Csvshov2View).FullName},
-                new ButtonConfig { Text = "JAN一括設定(49JAN含む)", WindowId="" },
+                new ButtonConfig { Text = "JAN一括設定(49JAN含む)", WindowId=typeof(Views.SubDlg01SetjanView).FullName,Parameter="0",Flag="0" },
                 new ButtonConfig { Text = "マスタ復帰処理", WindowId="" },
                 new ButtonConfig { Text = "商品色サイズメンテ", WindowId=typeof(Views.SubDlg01ShojanView).FullName },
                 new ButtonConfig { Text = "下札発行用CSVデータ作成", WindowId="" },
