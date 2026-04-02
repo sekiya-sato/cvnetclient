1e75d5ebe12d1f2f2291aaf11afe591256047bff
BellaZull <nabilahbintizulkifli@gmail.com>
2026-04-02 10:22:05 +0800

    店舗予算表の画面作りは完了

--- CvnetClient/Models/PageConfig.cs
+++ CvnetClient/Models/PageConfig.cs
@@ -79,7 +79,7 @@ namespace CvnetClient.Models
                 new ButtonConfig { Text = "販売員別予算マスタ", WindowId="" },
                 new ButtonConfig { Text = "営業担当別予算マスタ", WindowId=typeof(Views.SubDlg01yo3View).FullName,Parameter="0",Flag="0" },
                 new ButtonConfig { Text = "月別ﾌﾞﾗﾝﾄﾞｱｲﾃﾑ仕入予算マスタ", WindowId=typeof(Views.SubDlg30InpMbiView).FullName,Parameter="0",Flag="0" },
-                new ButtonConfig { Text = "店舗予算表", WindowId="" },
+                new ButtonConfig { Text = "店舗予算表",WindowId=typeof(Views.SubDlg30PrnYosanView).FullName,Parameter="0",Flag="0" },
                 new ButtonConfig { Text = "店舗別予算実績対比", WindowId="" },
                 new ButtonConfig { Text = "販売員予算表", WindowId="" },
                 new ButtonConfig { Text = "担当別売上予算実績半期報", WindowId=typeof(Views.SubDlg23PrnTantouhankiView).FullName,Parameter="0",Flag="0" },
